81b00393c02d0e70abc0566b47c466df68d0cac5
Tetiana Pavliuk <tpavliuk@corelogic.com>
2020-06-11 15:13:29 +0300

    Added hometask1 for Selenium WebDriver

---
+++ Selenium/src/test/java/FacebookTask.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FacebookTask {
+
+    private static WebDriver driver;
+
+    @Test
+    public void login() throws InterruptedException {
+        String exePath = "C:\\Users\\Tetiana_Pavliuk\\My_folder\\Java\\chromedriver_win32\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", exePath);
+        driver = new ChromeDriver();
+        String url = "http://facebook.com";
+        String login = "TesterJavaTraining";
+        String pass = "q1w2e3r4";
+        driver.get(url);
+        //Thread.sleep(10000);
+        WebElement submitButton = driver.findElement(By.xpath("//*[@type=\"submit\"]"));
+        driver.findElement(By.id("email")).sendKeys(login);
+        driver.findElement(By.id("pass")).sendKeys(pass);
+
+        submitButton.submit();
+        //Thread.sleep(10000);
+        assertFalse(submitButton.isDisplayed(), "You are not log in!");
+
+    }
+
+    @AfterAll
+    public static void quitDriver() {
+        driver.quit();
+    }
+}
