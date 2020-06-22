b1f167aff81bf41e39400dc453d961d8329305e4
Tetiana Pavliuk <tpavliuk@corelogic.com>
2020-06-22 16:18:18 +0300

    Added Home Task 4

---
+++ Selenium/src/test/java/WordNumberConverter.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordNumberConverter {
+    private static Map<String, Integer> map;
+
+    static {
+        map = new HashMap<>();
+        map.put("Zero", 0);
+        map.put("One", 1);
+        map.put("Two", 2);
+        map.put("Three", 3);
+        map.put("Four", 4);
+        map.put("Five", 5);
+        map.put("Six", 6);
+        map.put("Seven", 7);
+        map.put("Eight", 8);
+        map.put("Nine", 9);
+        map.put("Ten", 10);
+    }
+
+    public static Integer wordToNumber(String input) {
+        Integer output = null;
+        if (input != null) {
+            output = map.get(input);
+        }
+        return output;
+    }
+}
